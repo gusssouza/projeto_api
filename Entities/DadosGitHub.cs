6eeee7073ffcdcfc39bd9abd3d16652a461b10c2
Note-70 <Note-70@FULLTECHPRO-NOT>
2022-10-27 16:37:46 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/DadosGitHub.cs
@@ -0,0 +1,18 @@
+﻿
+namespace projeto_api.Entities
+{
+    public class DadosGitHub
+    {
+        public string login { get; set; }
+        public string avatar_url { get; set; }
+        public string repos_url { get; set; }
+        public bool site_admin { get; set; }
+        public string location { get; set; }
+        public string bio { get; set; }
+        public int followers { get; set; }
+        public int following { get; set; }
+        public string created_at { get; set;}
+        public string updated_at { get; set;}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using projeto_api.Entities;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace Projeto
+{
+    class ProjetoApi
+    {
+        public static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Insira o nome do usuário que você deseja pesquisar");
+            string user = Console.ReadLine();
+            string link = "https://api.github.com/users/";
+            RestClient restClient = new RestClient($"{link}{user}");
+            RestRequest restRequest = new RestRequest();
+            IRestResponse response = restClient.Execute(restRequest);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                Console.WriteLine("Erro");
+            }
+            else
+            {
+                DadosGitHub dados = new JsonDeserializer().Deserialize<DadosGitHub>(response);
+                Console.WriteLine("Login: " + dados.login);
+                Console.WriteLine("Avatar: " + dados.avatar_url);
+                Console.WriteLine("Repos: " + dados.repos_url);
+                Console.WriteLine("Site: " + dados.site_admin);
+                Console.WriteLine("Location: " + dados.location);
+                Console.WriteLine("Bio: " + dados.bio);
+                Console.WriteLine("Followers: " + dados.followers);
+                Console.WriteLine("Following: " + dados.following);
+                Console.WriteLine("Created: " + dados.created_at);
+                Console.WriteLine("Updated: " + dados.updated_at);
+            }
+        }
+    }
+}
